8bdf5b5c2a6b4a6dcd639458c81c00405ec90730
nolvertou <nolvertou@gmail.com>
2025-09-13 15:03:49 -0700

    Use DataStore to hold selected language

--- app/src/main/java/nou/com/example/translatecomposeapp/views/languages/LanguagesView.kt
+++ app/src/main/java/nou/com/example/translatecomposeapp/views/languages/LanguagesView.kt
@@ -12,26 +12,45 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import kotlinx.coroutines.launch
 import nou.com.example.translatecomposeapp.R
 
 @Composable
 fun LanguagesView(){
 
+    val context = LocalContext.current
+    val scope = rememberCoroutineScope()
+    val dataStore = LanguageStore(context)
+
+    // get index language from dataStore and save it at indexLang,
+    // initial value will be zero
+    val indexLang by dataStore.getIndexLang.collectAsState(initial = 0)
+
+    // Dropdown Variables
     val items = listOf("English", "Spanish")
     var expanded by remember { mutableStateOf(false) }
     var selectedIndex by remember { mutableIntStateOf(0) }
 
     var languages = getStrings()
-    var currentLanguage by remember { mutableStateOf(languages[0]) }
+    var currentLanguage by remember { mutableStateOf(languages[indexLang]) }
+
+    LaunchedEffect(indexLang) {
+        currentLanguage = languages[indexLang]
+        selectedIndex = indexLang
+    }
 
     Column(
         modifier = Modifier.fillMaxSize(),
@@ -53,6 +72,10 @@ fun LanguagesView(){
                         onClick = {
                             selectedIndex = index
                             currentLanguage = languages[index]
+                            scope.launch {
+                                dataStore.saveIndexLang(index)
+                            }
+
                             expanded = false
                         })
 
