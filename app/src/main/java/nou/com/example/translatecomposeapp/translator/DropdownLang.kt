ca2ed42c139e3365ba7cd1350ad1a0464f2da822
nolvertou <nolvertou@gmail.com>
2025-09-14 08:35:45 -0700

    Create a generic Dropdown Menu

--- app/src/main/java/nou/com/example/translatecomposeapp/translator/DropdownLang.kt
+++ app/src/main/java/nou/com/example/translatecomposeapp/translator/DropdownLang.kt
@@ -1,7 +1,38 @@
 package nou.com.example.translatecomposeapp.translator
 
+import android.text.Selection
+import androidx.compose.foundation.layout.Box
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 
 @Composable
-fun DropdownLang() {
+fun DropdownLang(
+    itemSelection: List<String>,
+    selectedIndex: Int,
+    expand: Boolean,
+    onCLickExpanded: () -> Unit,
+    onClickDismiss: () -> Unit,
+    onClickItem: (index: Int) -> Unit
+) {
+    Box(){
+        OutlinedButton(onClick = { onCLickExpanded() }) {
+            Text(text = itemSelection[selectedIndex])
+            Icon(imageVector = Icons.Default.ArrowDropDown, contentDescription = "Arrow DropDown")
+        }
+
+        DropdownMenu(expanded = expand, onDismissRequest = { onClickDismiss() }) {
+            itemSelection.forEachIndexed{ index, item ->
+                DropdownMenuItem(
+                    text = { Text(text = item) },
+                    onClick = { onClickItem(index) }
+                )
+            }
+        }
+    }
 }
